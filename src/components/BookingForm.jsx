c98a8bd9998a8bfb80ea2dc6144740d473760227
Fabian Soosaithasan <fabian.soosaithasan@gmail.com>
2024-08-27 12:34:09 +0200

    Add Booking component and store form data in state

---
+++ src/components/BookingForm.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import "../BookingForm.css";
+
+const BookingForm = ({ onDateChange, availableTimes }) => {
+  const [formData, setFormData] = useState({
+    date: "",
+    time: "",
+    guests: "",
+    occasion: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+    if (name === "date") {
+      onDateChange(value);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("formData", formData);
+  };
+
+  return (
+    <div className="container">
+      <form
+        className="bookingForm"
+        onSubmit={handleSubmit}
+        aria-labelledby="booking-form-title"
+      >
+        <h2 id="booking-form-title">Booking Form</h2>{" "}
+        <label htmlFor="res-date">Choose date</label>
+        <input
+          name="date"
+          type="date"
+          id="res-date"
+          value={formData.date}
+          onChange={handleChange}
+          required
+          aria-required="true"
+        />
+        <label htmlFor="res-time">Choose time</label>
+        <select
+          name="time"
+          id="res-time"
+          value={formData.time}
+          onChange={handleChange}
+          required
+          aria-required="true"
+          aria-label="Select reservation time"
+        >
+          <option value="" disabled>
+            Select a time
+          </option>
+          {availableTimes.map((time) => (
+            <option key={time} value={time}>
+              {time}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="guests">Number of guests</label>
+        <input
+          name="guests"
+          type="number"
+          placeholder="1"
+          min="1"
+          max="10"
+          id="guests"
+          value={formData.guests}
+          onChange={handleChange}
+          required
+          aria-required="true"
+          aria-label="Number of guests"
+        />
+        <label htmlFor="occasion">Occasion</label>
+        <select
+          name="occasion"
+          id="occasion"
+          value={formData.occasion}
+          onChange={handleChange}
+          required
+          aria-required="true"
+          aria-label="Select occasion"
+        >
+          <option value="">Select an occasion</option>
+          <option>Birthday</option>
+          <option>Anniversary</option>
+        </select>
+        <input
+          type="submit"
+          value="Make Your reservation"
+          aria-label="Submit reservation form"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default BookingForm;
---
+++ src/components/Hello.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "../Hello.css";
+
+const Hello = (props) => <h1 className="container">Hello {props.name} </h1>;
+
+export default Hello;
